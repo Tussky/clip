ec3fc68e353c3cf095c0fd73ddc77b58934ef643
tusky <isaac@dw-perspective.org.uk>
2025-11-17 23:16:40 -0500

    experimenting(uploading to Youtube): still no successful upload

--- src/to_youtube/upload_video.py
+++ src/to_youtube/upload_video.py
@@ -1,4 +1,25 @@
 import os
 
-# Link for future work
-# https://developers.google.com/youtube/v3/guides/uploading_a_video#python
+from apiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
+import json
+# loading in api key
+SECRET_FILE = "secrets.json"
+with open("client_secret.json") as secrets:
+    my_secrets = json.load(secrets)
+
+api_key = my_secrets['installed']['client_secret']
+client_id = my_secrets['installed']['client_id']
+
+
+youtube = build("youtube", "v3", developerKey = api_key) 
+
+request = youtube.channels().list(part="snippet", id =client_id) 
+
+response = request.execute()
+print(response)
