e34a34b42c4437999327e3a89585ddc8107a2540
tusky <isaac@dw-perspective.org.uk>
2025-11-22 20:10:02 -0500

    Feat(working on video upload - needs 02AUTH)

--- src/to_youtube/upload_video.py
+++ src/to_youtube/upload_video.py
@@ -11,6 +11,27 @@ from my_youtube_secrets import YT_OAUTHKEY, YT_API_KEY
 # building YT object and listing snippets
 youtube = build("youtube", "v3", developerKey = YT_API_KEY) 
 request = youtube.channels().list(part="snippet", id = YT_OAUTHKEY) 
-
 response = request.execute()
 print(response)
+
+
+media = MediaFileUpload("../../vids/tester_vid")
+# Attempting to upload a video
+# upload = youtube.videos().upload()
+
+body = {
+        "snippet": {
+            "title": "hi",
+            "description":"fun games",
+            "tags" : "fun",
+        }
+}
+
+upload_request = youtube.videos().insert(
+    part = "snippet,status",
+    body = body,
+    media_body = media,
+)
+print("created upload request")
+upload_response = upload_request.execute()
+print(upload_response)
